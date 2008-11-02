5b8e13d39ef8b1b492f55ffe4be30179cf2ae9c5
suma <devnull@localhost>
2008-11-03 06:40:03 +0900

    fixing HexConfig caluculation

--- src/control/standard/hexview.cpp
+++ src/control/standard/hexview.cpp
@@ -23,11 +23,23 @@ HexConfig::HexConfig()
 	Colors[0] = QColor(255,0,0);
 	Colors[1] = QColor(0,0,255);
 
+	for (int i = 1; i < Num; i++) {
+		Spaces[i] = FontMetrics.maxWidth();
+	}
+	Spaces[0] = 0;
+	Spaces[Num / 2] *= 2;
+
 	calculate();
 }
 
 void HexConfig::calculate()
 {
+	x_[0] = Margin.left() + ByteMargin.left();
+	for (int i = 1; i < Num; i++) {
+		x_[i] = x_[i-1] + byteWidth() + Spaces[i];
+	}
+
+	top_ = Margin.top() + ByteMargin.top();
 }
 
 ////////////////////////////////////////
@@ -39,14 +51,25 @@ HexView::HexView(QWidget *parent, Document *doc)
 
 void HexView::refreshPixmap()
 {
-	pix_.fill(getConfig().Colors[Color::Background]);
+	pix_.fill(config_.Colors[Color::Background]);
 
 	QPainter painter(&pix_);
 	//painter.initFrom(this);
 
+	// TODO: Optimizing drawing
+
 	// draw lines
+	int yCount = 5;
+	int y = config_.top();
+	for (int i = 0; i < yCount; i++) {
+		//drawLine
+		const int x = config_.x(i);
+		// draw hex
+		y += config_.byteHeight();
+	}
 
 	update();
 }
+
 
 }	// namespace
--- src/control/standard/hexview.h
+++ src/control/standard/hexview.h
@@ -10,21 +10,43 @@ namespace Standard {
 	class HexConfig
 	{
 	public:
+		enum {
+			Num = 16,
+		};
 		QRect Margin;
 		QRect ByteMargin;
 		QFont Font;
 		QColor Colors[16];
+		int Spaces[Num];
 
 	private:
 		QFontMetrics FontMetrics;
-		int x[16];
+		int top_;
+		int x_[Num];
 	
 	public:
 		HexConfig();
 
-		void updateFont()
+		inline void updateFont()
 		{
 			FontMetrics = QFontMetrics(Font);
+		}
+		inline int byteWidth() const
+		{
+			return ByteMargin.left() + FontMetrics.maxWidth() + ByteMargin.right();
+		}
+		inline int byteHeight() const
+		{
+			return ByteMargin.top() + FontMetrics.height() + ByteMargin.bottom();
+		}
+		inline int top() const
+		{
+			return top_;
+		}
+		inline int x(int i) const
+		{
+			Q_ASSERT(0 <= i && i < Num);
+			return x_[i];
 		}
 		void calculate();
 	};
