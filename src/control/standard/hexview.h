461f43e6d92547c8d00a13c89d7ee3493ec8efa9
suma <devnull@localhost>
2008-12-18 14:59:26 +0900

    fixing

--- src/control/standard/hexview.h
+++ src/control/standard/hexview.h
@@ -122,12 +122,12 @@ namespace Standard {
 		void isSelected(bool &selected, quint64 &sb, quint64 &se, quint64 top, int yCount, uint size);
 		void drawSelected(bool reset = false);
 		void drawCaret(bool visible, quint64 pos, int ytop, int ymax);
+		void redrawCaret();
 
 		void byteToHex(uchar c, QString &h);
 		quint64 moveByMouse(int x, int y);
 
 	public slots:
-		void redrawCaret();
 		void drawCaret(bool visible);
 		void drawCaret(bool visible, quint64 pos);
 
