64fb696446dff872c843cd3ce695f0de9283f8d4
suma <suma@users.sourceforge.jp>
2010-10-30 11:57:06 +0900

    implementing OriginalFile

--- src/control/document.cpp
+++ src/control/document.cpp
@@ -1,5 +1,6 @@
 
 #include <QFile>
+#include <limits>
 #include "document.h"
 #include "document_i.h"
 
@@ -17,9 +18,97 @@ public:
 		return 0;
 	}
 
-	void get(quint64 pos, uchar *buf, uint len) const
+	void get(quint64 pos, uchar *buf, quint64 len) const
 	{
 		// TODO: throw Exception
+	}
+};
+
+class FileOriginal : public DocumentOriginal
+{
+protected:
+	uint buffer_size_;
+
+	QFile *file_;
+
+	// pointer to mapped
+	mutable uchar *ptr_;
+
+	// mapped offset and size
+	mutable quint64 offset_;
+	mutable quint64 size_;
+	
+#define DEFAULT_BUFFER_SIZE 0x100000
+
+public:
+	FileOriginal(QFile *file, uint buffer_size = DEFAULT_BUFFER_SIZE)
+		: buffer_size_(buffer_size)		// 必ず16のN乗にする
+		, file_(file)
+		, offset_(0)
+		, size_(0)
+	{
+		// check buffer_size
+		uint size;
+		for (size = 1; size < 0x10000000; size <<= 1) {
+			if (size == buffer_size) {
+				break;
+			}
+		}
+		if (size == 0x10000000) {
+			buffer_size_ = (uint)DEFAULT_BUFFER_SIZE;
+		}
+		
+		remap(0);
+	}
+
+	virtual ~FileOriginal()
+	{
+		delete file_;
+	}
+
+	quint64 length() const
+	{
+		return file_->size();
+	}
+
+	void get(quint64 pos, uchar *buf, quint64 len) const
+	{
+		Q_ASSERT(pos < length() && len < length() - pos);
+
+		while (len > 0) {
+			if (!isOffsetCovered(pos)) {
+				remap(pos);
+			}
+		
+			// get start position
+			uint start = static_cast<uint>(pos & buffer_size_);
+			// get copy size
+			uint copy_size = qMin((quint64)buffer_size_ - start, len);
+
+			// copy to memory
+			memcpy(buf, ptr_ + start, copy_size);
+
+			pos += copy_size;
+			len -= copy_size;
+		}
+	}
+private:
+
+	void remap(quint64 pos) const
+	{
+		Q_ASSERT(pos < length());
+
+		offset_ = pos & ~(buffer_size_ - 1);
+
+		const quint64 map_size = qMin(length() - offset_, (quint64)buffer_size_);
+		ptr_ = file_->map(offset_, map_size, QFile::NoOptions);
+	}
+
+	bool isOffsetCovered(quint64 pos) const
+	{
+		Q_ASSERT(pos <= length() - size_);
+		const quint64 len = length();
+		return (offset_ <= pos && (pos >= len && offset_ < len - pos));
 	}
 };
 
@@ -70,11 +159,11 @@ void Document::get(quint64 pos, uchar *buf, uint len) const
 	Q_ASSERT(x != 0);
 	while (0 < len) {
 		const quint64 fragmentSize = impl_->documents_.size(x);
-		const uint cpSize = (static_cast<quint64>(len) < fragmentSize) ? len : static_cast<uint>(fragmentSize);
+		const uint copy_size = (static_cast<quint64>(len) < fragmentSize) ? len : static_cast<uint>(fragmentSize);
 		const DocumentData *X = impl_->documents_.fragment(x);
-		copy(X->type, X->bufferPosition, cpSize, buf);
-		len -= cpSize;
-		buf += cpSize;
+		copy(X->type, X->bufferPosition, copy_size, buf);
+		len -= copy_size;
+		buf += copy_size;
 		x = impl_->documents_.next(x);
 	}
 }
--- src/control/document.h
+++ src/control/document.h
@@ -11,7 +11,7 @@ class DocumentOriginal
 {
 public:
 	virtual quint64 length() const = 0;
-	virtual void get(quint64 pos, uchar *buf, uint len) const = 0;
+	virtual void get(quint64 pos, uchar *buf, quint64 len) const = 0;
 };
 
 class Document
@@ -25,11 +25,21 @@ public:
 	Document(const Document &doc, bool writemode);
 	virtual ~Document();
 
-	quint64 length() const;
 
+	// immutable methods
+	quint64 length() const;
 	void get(quint64 pos, uchar *buf, uint len) const;
+
+
+	// mutable methods
 	void insert(quint64 pos, const uchar *buf, uint len);
 	void remove(quint64 pos, quint64 len);
+
+	// TODO: undo/redo
+
+
+	// file method
+	//  save, saveAs, ....
 
 private:
 	void copy(uint type, quint64 pos, quint64 len, uchar *buf) const;
@@ -45,3 +55,4 @@ protected:
 
 
 #endif
+
