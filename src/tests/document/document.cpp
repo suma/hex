52a7d3a1c1dc2e388189dcb40544b048661e5297
suma <suma@users.sourceforge.jp>
2010-11-03 22:58:41 +0900

    implementing doc test

--- src/tests/document/document.cpp
+++ src/tests/document/document.cpp
@@ -1,18 +1,70 @@
 #include <QtTest/QtTest>
+#include <QTemporaryFile>
+#include <QDataStream>
+#include <vector>
 #include "control/document.h"
 
 class TestDocument: public QObject
 {
 	Q_OBJECT
 private slots:
-	void toUpper();
+	void initTestCase();
+	void openFile();
+	void cleanupTestCase();
+	void open();
+private:
+	QTemporaryFile *temp_file_;
+	QFile *file_;
+	std::vector<char> data_;
 };
 
-void TestDocument::toUpper()
+const int SIZE = 1024 * 1024 * 16;	// 64MB
+
+void TestDocument::initTestCase()
 {
-	Document *doc = new Document();
-	QString str = "Hello";
-	QCOMPARE(str.toUpper(), QString("HELLO"));
+	// write to dummy file
+	data_.resize(SIZE);
+	qsrand(107);
+	for (int i = 0; i < SIZE; i++) {
+		data_.push_back(static_cast<char>(qrand() & 0xFF));
+	}
+
+	temp_file_ = new QTemporaryFile();
+	QVERIFY2(temp_file_->open(), "file open failed");
+
+	temp_file_->setAutoRemove(true);
+
+	open();
+	QVERIFY(file_->seek(0));
+
+	QDataStream outStream(file_);
+	QVERIFY(outStream.writeRawData(&data_[0], SIZE) == SIZE);
+	//QVERIFY(file_->writeData(&data_[0], SIZE) == SIZE);
+}
+
+void TestDocument::cleanupTestCase()
+{
+	// delete dummy file
+	delete temp_file_;
+
+}
+
+void TestDocument::open()
+{
+	file_ = new QFile(temp_file_->fileName());
+	QVERIFY2(file_->open(QIODevice::ReadWrite), "file open failed");
+}
+
+void TestDocument::openFile()
+{
+	QFile *file = new QFile(QString("./tests"));
+	Document *doc = new Document(file);
+
+	// copy data
+	
+	// verify data
+
+	delete doc;
 }
 
 QTEST_MAIN(TestDocument)
