86f387e98160731d517b5acb2faed84032cb0260
suma <suma@users.sourceforge.jp>
2013-07-30 01:12:05 +0900

    Update document_i.cpp based on Qt 4.8

--- src/control/document_i.cpp
+++ src/control/document_i.cpp
@@ -1,45 +1,50 @@
 /****************************************************************************
 **
-** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
-** Contact: Qt Software Information (qt-info@nokia.com)
+** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
 **
 ** This file is part of the QtGui module of the Qt Toolkit.
 **
-** No Commercial Usage
-** This file contains pre-release code and may not be distributed.
-** You may use this file in accordance with the terms and conditions
-** contained in the either Technology Preview License Agreement or the
-** Beta Release License Agreement.
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
 **
 ** GNU General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU
-** General Public License versions 2.0 or 3.0 as published by the Free
-** Software Foundation and appearing in the file LICENSE.GPL included in
-** the packaging of this file.  Please review the following information
-** to ensure GNU General Public Licensing requirements will be met:
-** http://www.fsf.org/licensing/licenses/info/GPLv2.html and
-** http://www.gnu.org/copyleft/gpl.html.  In addition, as a special
-** exception, Nokia gives you certain additional rights. These rights
-** are described in the Nokia Qt GPL Exception version 1.3, included in
-** the file GPL_EXCEPTION.txt in this package.
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
 **
-** Qt for Windows(R) Licensees
-** As a special exception, Nokia, as the sole copyright holder for Qt
-** Designer, grants users of the Qt/Eclipse Integration plug-in the
-** right for the Qt/Eclipse Integration to link to functionality
-** provided by Qt Designer and its related libraries.
 **
-** If you are unsure which license is appropriate for your use, please
-** contact the sales department at qt-sales@nokia.com.
+** $QT_END_LICENSE$
 **
 ****************************************************************************/
-/* original by src/gui/text/qtextdocument_p.cpp */
+
+// This file is derived by
+// http://qt.gitorious.org/qt/qt/blobs/raw/4.8/src/gui/text/qtextdocument_p.cpp
 
 #include "document_i.h"
 
-
 DocumentImpl::DocumentImpl()
-	: length_(0)
 {
 }
 
@@ -47,61 +52,66 @@ DocumentImpl::~DocumentImpl()
 {
 }
 
-uint DocumentImpl::insert_data(ulint pos, ulint bufPos, ulint len, quint8 type)
+uint DocumentImpl::insert_data(ulint pos, ulint bufPos, ulint length, quint8 type)
 {
-	Q_ASSERT(len != 0);
+    Q_ASSERT(length != 0);
 
     split(pos);
-    uint x = documents_.insert_single(pos, len);
+    uint x = documents_.insert_single(pos, length);
+
     DocumentData *X = documents_.fragment(x);
     X->type = type;
     X->bufferPosition = bufPos;
-
-	length_ += len;
-
     uint w = documents_.previous(x);
-    if (w) {
+    if (w)
         unite(w);
-	}
-	return w;
+    return w;
 }
 
-uint DocumentImpl::remove_data(ulint pos, ulint len)
+uint DocumentImpl::remove_data(qint64 pos, ulint length)
 {
-	Q_ASSERT(len != 0);
+    Q_ASSERT(pos >= 0 && pos+length <= documents_.length());
 
-	//qDebug("remove_data pos:%llu len:%llu", pos, len);
+    uint x = documents_.findNode(pos);
+    return move(pos, -1, length);
+}
+
+uint DocumentImpl::move(qint64 pos, qint64 to, ulint length)
+{
+    Q_ASSERT(to <= documents_.length() && to <= pos);
+    Q_ASSERT(pos >= 0 && pos+length <= documents_.length());
+
+    if (pos == to)
+        return 0;
+    const bool needsInsert = to != -1;
+
     split(pos);
-    split(pos+len);
-	uint x = documents_.findNode(pos);
-    uint w = documents_.previous(x);
+    split(pos+length);
 
-	Q_ASSERT(x != 0);
-	while (0 < len) {
-		ulint size = documents_.size(x);
-		if (size < len) {
-			//qDebug("pos:%llu x:%u size:%llu len:%llu", pos, x, size, len);
-			len -= size;
-			length_ -= size;
-			x = documents_.erase_single(x);
-			//qDebug("erased x:%u", x);
-			if (!x) {
-				x = documents_.findNode(pos);
-			} else {
-				x = documents_.next(x);
-			}
-			//qDebug("next  x:%u", x);
-		} else {
-			length_ -= len;
-			documents_.setSize(x, size - len);
-			break;
-		}
-	}
+    uint dst = needsInsert ? documents_.findNode(to) : 0;
+    ulint dstPos = to;
 
-    if (w) {
+    uint x = documents_.findNode(pos);
+    uint end = documents_.findNode(pos+length);
+    uint w = 0;
+    while (x != end) {
+        uint n = documents_.next(x);
+
+        w = documents_.erase_single(x);
+
+        if (needsInsert) {
+            // TODO(suma): Write move test
+            DocumentData *X = documents_.fragment(x);
+            insert_data(dstPos, X->bufferPosition, X->size_array[0], X->type);
+            dstPos += X->size_array[0];
+        }
+
+        x = n;
+    }
+    if (w)
         unite(w);
-	}
-	return w;
+
+    return w;
 }
 
 bool DocumentImpl::split(ulint pos)
@@ -110,7 +120,7 @@ bool DocumentImpl::split(ulint pos)
     if (x) {
         ulint k = documents_.position(x);
 //          qDebug("found fragment with key %d, size_left=%d, size=%d to split at %d",
-//                k, (*it)->size_left, (*it)->size, pos);
+//                k, (*it)->size_left[0], (*it)->size_array[0], pos);
         if (k != pos) {
             Q_ASSERT(k <= pos);
             // need to resize the first fragment and add a new one
@@ -127,7 +137,6 @@ bool DocumentImpl::split(ulint pos)
     }
     return false;
 }
-
 
 bool DocumentImpl::unite(uint f)
 {
@@ -148,4 +157,3 @@ bool DocumentImpl::unite(uint f)
     }
     return false;
 }
-
